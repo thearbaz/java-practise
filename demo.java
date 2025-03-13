5edd931ecaa8a8b1d9b12c61a479f4663508b7fc
nlyy586s <arbazahmad@Arbazmackbookair.local>
2025-03-13 12:20:25 +0530

    modified

--- demo.java
+++ demo.java
@@ -1,10 +1,10 @@
 public class demo {
     public static void main(String[] args) {
-        int N = 10; // Change this value to calculate the sum of first N natural numbers
+        int N = 10;  
         int sum = 0;
 
         for (int i = 1; i <= N; i++) {
-            sum += i; // Add i to sum
+            sum += i; 
         }
 
         System.out.println("Sum of first " + N + " natural numbers is: " + sum);
