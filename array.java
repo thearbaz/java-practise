0c4082e043f75a11bb42b3832a32caee2ec715d5
nlyy586s <arbazahmad@Arbazmackbookair.local>
2025-03-19 21:14:20 +0530

    array

---
+++ array.java
@@ -0,0 +1,22 @@
+public class array 
+{
+    public static void main(String a[])
+    {
+
+            int nums[] = new int[4];
+            nums[0] =1;
+            nums[1] = 5;
+            nums[2] = 8;
+            nums[3] = 10;
+
+            for(int i=0; i<4; i++)
+            {
+                System.out.println(nums[i]);
+            }
+
+
+
+            
+
+    }
+}
